286020dd7c7e8d5cc3801e4cdfec6889a65f0a01
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2025-03-01 12:47:30 +0530

    add Text field

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,13 +6,45 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
  @override
  Widget build(BuildContext context){
   return const Scaffold(
+      backgroundColor:Color.fromARGB(255, 153, 148, 148) ,
       body:Center(
        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("0")
+
+          Text("0",style:TextStyle(
+            fontSize: 55 ,        // Font size
+            fontWeight: FontWeight.bold,  // Font weight (e.g., bold)
+            color: Color.fromARGB(255,255,255,255),  // Text color
+            fontStyle: FontStyle.italic, // Italicize the text
+            letterSpacing: 2.0,  // Space between letters
+            height: 1.5,         // Line height
+          ) ,),
+
+          TextField(style: TextStyle(
+            color: Color.fromARGB(255, 10, 10, 10),
+          ),
+            decoration: InputDecoration(
+              hintText:  'Please enter the Amount in LKR',
+              hintStyle: TextStyle(
+                color: Colors.black,
+            ),   
+              prefixIcon: Icon(Icons.monetization_on),  
+              prefixIconColor: Colors.black,
+              filled: true,
+              fillColor:Colors.white, 
+              focusedBorder:OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  width: 2.0,
+                  style: BorderStyle.solid,
+                  strokeAlign: BorderSide.strokeAlignCenter,
+                ),),           
+           ),              
+          ),
         ],
        ),
-        
+         
       )
   );
  }
