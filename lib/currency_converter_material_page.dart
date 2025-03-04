78860bdfd7c5e6730b422d74f010547cd45ea0f8
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2025-03-04 16:41:13 +0530

    add statefulwidget

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,18 +1,46 @@
+
+
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget{
  const CurrencyConverterMaterialPage({super.key});
-
+ 
+ 
  @override
  Widget build(BuildContext context){
-  return const Scaffold(
-      backgroundColor:Color.fromARGB(255, 153, 148, 148) ,
+  double result=0;
+  final TextEditingController textEditingController=TextEditingController();
+
+   const border= OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  width: 2.0,
+                  style: BorderStyle.solid,
+                  strokeAlign: BorderSide.strokeAlignCenter,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(60)),
+             );
+  return Scaffold(
+      backgroundColor:const Color.fromARGB(255, 153, 148, 148) ,
+      appBar: AppBar(
+      backgroundColor:const Color.fromARGB(255, 153, 148, 148) , 
+      elevation: 0, 
+      title: const Text(
+        'Currency Converter',
+        style: TextStyle(
+          color: Colors.black,
+        ),  
+      
+      ),
+      centerTitle: true,
+      ),
       body:Center(
        child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
 
-          Text("0",style:TextStyle(
+           Text(result.toString(),
+          style: const TextStyle(
             fontSize: 55 ,        // Font size
             fontWeight: FontWeight.bold,  // Font weight (e.g., bold)
             color: Color.fromARGB(255,255,255,255),  // Text color
@@ -21,27 +49,49 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
             height: 1.5,         // Line height
           ) ,),
 
-          TextField(style: TextStyle(
-            color: Color.fromARGB(255, 10, 10, 10),
-          ),
-            decoration: InputDecoration(
-              hintText:  'Please enter the Amount in LKR',
-              hintStyle: TextStyle(
-                color: Colors.black,
-            ),   
-              prefixIcon: Icon(Icons.monetization_on),  
-              prefixIconColor: Colors.black,
-              filled: true,
-              fillColor:Colors.white, 
-              focusedBorder:OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  width: 2.0,
-                  style: BorderStyle.solid,
-                  strokeAlign: BorderSide.strokeAlignCenter,
-                ),),           
-           ),              
-          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: textEditingController,
+              style: const TextStyle(
+              color: Color.fromARGB(255, 10, 10, 10),
+            ),
+              decoration: const InputDecoration(
+                hintText:  'Please enter the Amount in LKR',
+                hintStyle: TextStyle(
+                  color: Colors.black,
+              ),   
+                prefixIcon: Icon(Icons.monetization_on),  
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor:Colors.white, 
+                focusedBorder:border,
+                enabledBorder: border,
+               ),
+                keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+               ),         
+             ),
+          ),              
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {               
+                 result=double.parse(textEditingController.text)*300;
+                 build(context);
+              },
+              style:  ButtonStyle( 
+                elevation: const WidgetStatePropertyAll(15),
+                backgroundColor: const WidgetStatePropertyAll(Colors.black),
+                foregroundColor: const WidgetStatePropertyAll(Colors.white),
+                shape:WidgetStatePropertyAll(BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                )),
+                
+              ),
+               child: const Text('Convert'),
+               ),
+            ),
         ],
        ),
          
